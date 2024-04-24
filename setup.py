b7bb49d0e52d84229ade069c3e8a320917333030
Yiyi Yang <57012409+yiyiyang81@users.noreply.github.com>
2024-04-23 20:08:37 -0400

    Create MLFlow Set Up for Models (#31)
    * reorganize repo
    
    * update preprocessing
    
    * update poetry config file
    
    * update README.md
    
    * add dependency lock
    
    * intergrate mlflow
    
    ---------
    
    Co-authored-by: Emily Wu <emily.wu2@mail.mcgill.ca>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='using the Spotify dataset to analyze the features of songs and their popularity, predict song popuparity and causal model for song duration effect on its popularity',
+    author='INSY695',
+    license='MIT',
+)
